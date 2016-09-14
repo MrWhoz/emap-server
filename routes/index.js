3797b70140f425c61c78c4b63bd51cd2b3688142
MrWhoz <mr.death.305@gmail.com>
2016-09-14 08:41:21 +0700

    add rethinkdb module

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var node = require('../models/node.js');
 router.get('/', function(req,res,next){
   res.render('index');
+  node.connect();
 })
 
 module.exports = router;
