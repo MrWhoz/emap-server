514079fee9703d7677bf9cd73099e00f05e5f148
manhcuong3010 <reaper.darkness3010@gmail.com>
2016-10-12 23:47:20 +0700

    session completed

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.set('view engine', 'ejs');
 
 app.use(session({secret:'ssshhhh'}));
 var sess;
+var checksess;
 
 app.get('/check',function(req,res){
     sess = req.session;
@@ -33,7 +34,11 @@ app.get('/check',function(req,res){
     }
 });
 
-
+// app.get('/contact',function(req,res){
+//       res.render('contact',{
+//         title: 'contact', temp:'nouser'
+//       })
+// });
 // check session 
 app.post('/login',function(req,res){
   sess = req.session;
@@ -45,30 +50,110 @@ app.use(function(req, res, next){
     res.locals.session = "hi";
     next();
 });
-app.get('/stastic',function(req,res){
+
+app.get('/home',function(req,res){
+  // sess=req.session;
   sess=req.session;
-  if(sess.email){
-    res.render('stastic', {
-        title: 'test', temp:sess.email
+  checksess = sess.email;
+  if(checksess){
+    res.render('home', {
+        title: 'test', temp:checksess
     });
     
 
   }else{
-    // res.write('<h1>Please login first.</h1>');
-    // res.end('<a href="/check">Login</a>');
+    res.render('home', {
+        title: 'test',
+        temp: 'nouser'
+    });
+    
+  }
+});
+app.get('/stastic',function(req,res){
+  if(checksess){
+    res.render('stastic', {
+        title: 'test', temp:checksess
+    });
+    app.get('/contact',function(req,res){
+      if(sess.email){
+        res.render('contact',{
+          title: 'contact', temp:checksess
+        })
+      }
+    });
+    
+
+  }else{
     res.render('stastic', {
         title: 'test',
         temp: 'nouser'
     });
+    
   }
 });
+
+//------------
+app.get('/contact',function(req,res){
+  // sess=req.session;
+  if(checksess){
+    res.render('contact', {
+        title: 'test', temp:checksess
+    });
+    
+
+  }else{
+    res.render('contact', {
+        title: 'test',
+        temp: 'nouser'
+    });
+    
+  }
+});
+//-----Graph session
+app.get('/graph',function(req,res){
+  // sess=req.session;
+  if(checksess){
+    res.render('graph', {
+        title: 'test',qs: req.query, temp:checksess
+    });
+    
+
+  }else{
+    res.render('graph', {
+        title: 'test',
+        temp: 'nouser',
+        qs: req.query
+    });
+    
+  }
+});
+//----home page session
+//-----Graph session
+app.get('/',function(req,res){
+  // sess=req.session;
+  if(checksess){
+    res.render('home', {
+        title: 'test', temp:checksess
+    });
+    
+
+  }else{
+    res.render('home', {
+        title: 'test',
+        temp: 'nouser'
+    });
+    
+  }
+});
+
+
 
 app.get('/logout',function(req,res){
   req.session.destroy(function(err){
     if(err){
       console.log(err);
     }else{
-      res.redirect('/stastic');
+      res.redirect('/home');
     }
   })
 });
