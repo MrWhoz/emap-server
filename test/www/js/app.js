52409db3841a70efd1055fed0f68c25f93bfe8ab
manhcuong3010 <reaper.darkness3010@gmail.com>
2016-10-18 22:29:00 +0700

    a

--- test/www/js/app.js
+++ test/www/js/app.js
@@ -3,9 +3,59 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('newsApp', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.controller('myNewsController', function($scope, $http){
+
+  $scope.news = [];
+
+  $scope.loadMore = function(){
+
+    var parameters = {
+        id:$scope.lastarticleID
+    }
+
+    $http.get('http://codedamn.com/filesCodedamn/news.php',{params: parameters}).success(function(items){
+        $scope.lastarticleID = items.lastID;
+        angular.forEach(items, function(item){
+          $scope.news.push(item);
+        })
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
+
+  $scope.doRefresh = function() {
+    $scope.news = [];
+    $http({
+          method: "GET",
+          url: "http://codedamn.com/filesCodedamn/news.php"
+        }).then(function(newsData){
+          angular.forEach(newsData.data, function(newsArticle){
+              $scope.news.push(newsArticle);
+          });
+          $scope.news.lastarticleID = newsData.data.lastID;
+          console.log($scope.news.lastarticleID);
+          $scope.lastarticleID = newsData.data.lastID;
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+       // Stop the ion-refresher from spinning
+       
+    
+  };
+
+  $http({
+    method: "GET",
+    url: "http://codedamn.com/filesCodedamn/news.php"
+  }).then(function(newsData){
+    angular.forEach(newsData.data, function(newsArticle){
+        $scope.news.push(newsArticle);
+    });
+    $scope.news.lastarticleID = newsData.data.lastID;
+    console.log($scope.news.lastarticleID);
+  });
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
