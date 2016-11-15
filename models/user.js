94f761f01da2849173646d650469a42ebdb68989
MrWhoz <mr.death.305@gmail.com>
2016-11-15 21:48:19 +0700

    init user database, add get function

--- models/user.js
+++ models/user.js
@@ -7,3 +7,46 @@ async function connect() {
     connection = await r.connect(config.rethinkdb);
     return connection;
 };
+
+async function register(user) {
+  let connection = await connect();
+  // Check if user exist
+  let existId = await getUserById(user.id);
+  if (existId) {
+    return {
+      code: -1,
+      message: "Username đã tồn tại, vui lòng chọn username khác",
+      data: existId
+    };
+  };
+
+  var data = {
+    user_id: user.id,
+    password: user.password,
+    role: user.role,
+    full_naem: user.name,
+    created_time: new Date()
+  };
+}
+
+async function getUserById(id) {
+	var connection = await connect();
+	var user = await r.db(dbName).table("user").filter({user_id:id}).run(connection);
+  user = await user.toArray();
+  return user[0] || false;
+}
+
+async function getUserList(status) {
+    var connection = await connect();
+    var user = await r.db(dbName).table("user").filter({
+        status: status
+    }).run(connection);
+    user = await user.toArray();
+    return user || false;
+}
+
+module.exports = {
+	register,
+  getUserById,
+  getUserList
+}
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var session = require('express-session');
 var sockio = require("socket.io");
-var sess;
-// -----
 var router = express.Router();
+var passport = require('passport');
+var user = require('../models/user.js');
 
-router.get('/', function(req, res, next) {
-    res.send('USER ROUTE');
+router.get('/', async function(req, res, next) {
+  console.log('dsadasasd');
+    var userList = await user.getUserList('active');
+
+
+    console.log(userList);
 });
+
+router.post('/login', async function(req, res, next) {
+
+});
+
 
 module.exports = router;
